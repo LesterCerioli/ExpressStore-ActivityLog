2e3a04d6e001991b0c62e44b9eb35201946b1f58
Ana Ramos <anapauladeoliveiraramos@yahoo.com.br>
2022-10-03 14:28:13 -0300

    "feat(342) implementing modules into infrastructure"

---
+++ source/Infrastructure/ExpressStore.Infrastructure/Modules/IModuleInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressStore.Infrastructure.Modules
+{
+    public interface IModuleInitializer
+    {
+        void ConfigureServices(IServiceCollection serviceCollection);
+
+        void Configure(IApplicationBuilder app, IWebHostEnvironment env);
+    }
+}
